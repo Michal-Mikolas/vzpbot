e61f2926909cbed9dd018f3389a5165b7c861a6a
Michal Mikolas <nanuqcz@gmail.com>
2023-12-21 15:46:17 +0100

    fixed missing changes

--- vzpbot.py
+++ vzpbot.py
@@ -4,25 +4,27 @@ import time
 class VZPBot(object):
 
 	def init(self):
-		start_firefox()
+		# start_firefox()
+		start_chrome()
 		go_to('https://auth.vzp.cz/signin')
-		click(u'Přihlásit se certifikátem')
+		click(u'CERTIFIKÁT')
 		wait_until(S('h2.heading.heading--medium.u-mb-1h').exists, timeout_secs=120)
 
 		go_to('https://point.vzp.cz/online/online01')
-		click(u'Ověřit pojištěnce dle jména a data narození')
+		wait_until(lambda: self.shadow('#queryBy_radioinput_8'))
+		click(self.shadow('#queryBy_radioinput_8'))
 
 	def fetch_insurance(self, data):
-		write(data['name'], S('#Search_FirstName'))
-		write(data['surname'], S('#Search_LastName'))
-		write(data['birthdate'].strftime('%d.%m.%Y'), S('#Search_BirthDate'))
-		click(S('.active.start-date.active.end-date.available'))  # confirm birthdate
+		write(data['name'], self.shadow('[name=firstName]'))
+		write(data['surname'], self.shadow('[name=lastName]'))
+		write(data['birthdate'].strftime('%d.%m.%Y'), self.shadow('[name=birthDate]'))
+		# click(self.shadow('.active.start-date.active.end-date.available'))  # confirm birthdate
 
-		click('Ověřit pojištění')
+		click(self.shadow('.btn.btn-primary.btn-lg'))
 
-		wait_until(S('#result').exists)
+		wait_until(lambda: self.shadow('.form-container > div.well:nth-child(5)'))
 
-		data['insurance_text'] = S('#result').web_element.text.replace(u'Výsledek ověření', '').strip()
+		data['insurance_text'] = self.shadow('.form-container > div.well:nth-child(5)').text.replace(u'Výsledek ověření', '').strip()
 		if u'platné pojištění' in data['insurance_text']:
 			data['insurance_type'] = 1
 		elif u'žádný pojištěnec' in data['insurance_text']:
@@ -35,3 +37,8 @@ class VZPBot(object):
 			data['insurance_type'] = 'ERROR'
 
 		return data
+
+	def shadow(self, query):
+		shadowQuery = """return document.querySelector('#content-panel > div').shadowRoot.querySelector('{}');"""
+		return get_driver().execute_script(shadowQuery.format(query))
+data
